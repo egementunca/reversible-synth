22b58228ddba4b61a09f24824bf104c2b928f67c
egementunca <45570897+egementunca@users.noreply.github.com>
2025-12-16 18:08:08 -0500

    Create reversible_synth/__init__.py

---
+++ reversible_synth/__init__.py
@@ -0,0 +1,13 @@
+"""
+Reversible Circuit Synthesizer for Custom Universal Gate
+Gate: Target XOR (Control1 OR NOT Control2)
+"""
+
+__version__ = "0.1.0"
+__all__ = ['Permutation', 'CustomGate', 'Circuit', 'ExactSynthesizer', 'TransformationSynthesizer', 'IdentityGenerator']
+
+from .permutation import Permutation
+from .gates import CustomGate, Circuit
+from .synthesis_exact import ExactSynthesizer
+from .synthesis_heuristic import TransformationSynthesizer
+from .identity_generator import IdentityGenerator
